dcedc78784dd06861a043818944383d14cec4d1d
LeTamanoir <martin.saldinger@gmail.com>
2023-05-13 19:02:01 +0200

    feat: :sparkles: add file upload to ipfs

--- frontend/sdk/index.tsx
+++ frontend/sdk/index.tsx
@@ -3,9 +3,13 @@ import TKicksmarterCtx from "./types/TKicksmarterCtx";
 import TProject from "./types/TProject";
 import { useTezosContext } from "@/src/contexts/TezosContext";
 import { TMethodsExternal } from "./types/TExternal";
-import { _getProject, _getStorage } from "./utils";
+import {
+  _getProject,
+  _getStorage,
+  pushImageToIPFS,
+  pushMetadataToIPFS,
+} from "./utils";
 import TMilestone from "./types/TMilestone";
-import { PINATA_PUBLIC_GATEWAY_URL } from "@/src/constants";
 
 const KicksmarterCtx = createContext<TKicksmarterCtx>({} as TKicksmarterCtx);
 const useKickSmarter = () => useContext(KicksmarterCtx);
@@ -18,31 +22,6 @@ const KicksmarterProvider = ({
   children: React.ReactNode;
 }) => {
   const { tezos, connected } = useTezosContext();
-
-  const pushMetadataToIPFS = async ({
-    title,
-    description,
-  }: {
-    title: string;
-    description: string;
-  }): Promise<string> => {
-    let payload = JSON.stringify({
-      pinataOptions: { cidVersion: 1 },
-      pinataContent: { title, description },
-    });
-
-    const res = await fetch("/api/ipfs", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: payload,
-    });
-
-    const data = await res.json();
-
-    return data.ipfsHash;
-  };
 
   const getProject = async (project_id: number): Promise<TProject> => {
     let storage = await _getStorage(tezos!, contractAddress);
@@ -117,6 +96,7 @@ const KicksmarterProvider = ({
     <KicksmarterCtx.Provider
       value={{
         pushMetadataToIPFS,
+        pushImageToIPFS,
         getProjects,
         getProject,
         postProject,
